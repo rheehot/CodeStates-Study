9411efc04bd4f6b4f1124513f5182e3c5254c686
TheJang <33803975+jangwonyoon@users.noreply.github.com>
2020-03-20 18:22:39 +0900

    Update linked-list.js

--- Immersive/1주차/Data _structure/Data _structure/linked-list.js
+++ Immersive/1주차/Data _structure/Data _structure/linked-list.js
@@ -1,16 +1,14 @@
-```js
-
 const LinkedList = function() {
   const list = {};
   // 가장 먼저 들어온 Node;
   list.head = null;
   // 마지막에 들어온 Node;
   list.tail = null;
-
+  
   list.addToTail = function(value) {
-    var newNode = new Node(value);
+    var newNode = new Node(value); //새로운 노드 생성 
     if (list.tail) {
-      list.tail.next = newNode;
+      list.tail.next = newNode; 
       list.tail = newNode;
     } else {
       list.head = newNode;
@@ -55,5 +53,3 @@ const Node = function(value) {
  * Complexity: What is the time complexity of the above functions?
  */
 module.exports = LinkedList;
-
-```
