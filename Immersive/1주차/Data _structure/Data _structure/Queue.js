d834fce699c35a893e243412531d90af6f32a015
TheJang <33803975+jangwonyoon@users.noreply.github.com>
2020-03-21 15:58:09 +0900

    Update Queue.js

--- Immersive/1주차/Data _structure/Data _structure/Queue.js
+++ Immersive/1주차/Data _structure/Data _structure/Queue.js
@@ -31,8 +31,74 @@ const Queue = function() {
   return someInstance;
 };
 
-if (typeof module === 'object' && typeof module.exports === 'object') {
-  module.exports = {
-    Queue,
+
+// functional- shared 방식
+
+const Queue = function() {
+  var someInstance = {
+    front: 0,
+    rear: 0,
+    storage: {}
   };
-}
+  extend(someInstance, queueMethods);
+  return someInstance;
+};
+
+const queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.rear = this.rear + 1;
+  this.storage[this.rear] = value;
+};
+
+queueMethods.dequeue = function() {
+  if (this.rear === this.front) {
+    return "empty";
+  }
+  this.front = this.front + 1;
+  return this.storage[this.front];
+};
+
+queueMethods.size = function() {
+  return this.rear - this.front;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+
+// prototypal 방식
+
+const Queue = function() {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  var queueInstance = Object.create(queueMethods);
+  return queueInstance;
+};
+
+const queueMethods = {
+  rear: 0,
+  front: 0,
+  storage: {}
+};
+
+queueMethods.enqueue = function(value) {
+  this.rear = this.rear + 1;
+  this.storage[this.rear] = value;
+};
+
+queueMethods.dequeue = function() {
+  if (this.front === this.rear) {
+    return "empty";
+  }
+  this.front = this.front + 1;
+  return this.storage[this.front];
+};
+
+queueMethods.size = function() {
+  return this.rear - this.front;
+};
+
