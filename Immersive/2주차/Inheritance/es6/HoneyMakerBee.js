5658d2192f935d1dff83827d4a74041083830a13
TheJang <33803975+jangwonyoon@users.noreply.github.com>
2020-03-26 19:59:14 +0900

    Add files via upload

---
+++ Immersive/2주차/Inheritance/es6/HoneyMakerBee.js
@@ -0,0 +1,19 @@
+const Bee = require("./Bee");
+
+class HoneyMakerBee extends Bee {
+  // Bee 상속
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = "make honey";
+    this.honeyPot = 0;
+  }
+  makeHoney() {
+    this.honeyPot += 1;
+  }
+  giveHoney() {
+    this.honeyPot -= 1;
+  }
+}
+
+module.exports = HoneyMakerBee;
---
+++ Immersive/2주차/Inheritance/es6/RetiredForagerBee.js
@@ -0,0 +1,19 @@
+const ForagerBee = require("./ForagerBee");
+
+class RetiredForagerBee extends ForagerBee {
+  constructor() {
+    super();
+    this.age = 40;
+    this.job = "gamble";
+    this.canFly = false;
+    this.color = "grey";
+  }
+  forage() {
+    return "I am too old, let me play cards instead";
+  }
+  gamble(value) {
+    this.treasureChest.push(value);
+  }
+}
+
+module.exports = RetiredForagerBee;
