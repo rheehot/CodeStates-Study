139e463fb8c478514271dfca4d525788563c46d5
TheJang <33803975+jangwonyoon@users.noreply.github.com>
2020-03-31 10:00:28 +0900

    Add files via upload

---
+++ _Algorithm/Immersive_algorithm/isSubsetOf.js
@@ -0,0 +1,48 @@
+/*
+ * 특정 배열이 매개변수로 전달되는 배열의 부분집합인지를 판단하는 메소드를 만드세요.
+ * 비교 대상인 두 배열 모두 문자열로 이루어졌다고 가정합니다.
+ *
+ * var a = ['commit','push']
+ * a.isSubsetOf(['commit','rebase','push','blame']) // true
+ *
+ * 참고 : 배열 내의 중복값은 무시하세요.
+ *
+ * var b = ['merge','reset','reset']
+ *
+ * b.isSubsetOf(['reset','merge','add','commit']) // true
+ *
+ * 부분집합(subset)의 정의에 대한 자세한 정보는 여기(http://ko.wikipedia.org/wiki/부분집합)를 참고하세요.
+ *
+ */
+
+/*
+ * Advanced : 메소드가 문자열 이외의 자료형을 포함한 배열에도 작동하도록 개선해 보세요.
+ */
+
+// 풀이 1
+// Array.prototype.isSubsetOf = function(array) {
+//   return this.every(function(el) {
+//     return array.includes(el);
+//   });
+// };
+
+/* 풀이 2 
+Array.prototype.isSubsetOf = function(array) {
+  for (let i = 0; i < this.length; i++) {
+    if (array.indexOf(this[i]) === -1) {
+      return false;
+    }
+  }
+  return true;
+};
+*/
+
+Array.prototype.isSubsetOf = function(array) {
+  let inputArr = JSON.stringify(array);
+  for (let i = 0; i < this.length; i++) {
+    if (inputArr.indexOf(JSON.stringify(this[i])) === -1) {
+      return false;
+    }
+  }
+  return true;
+};
