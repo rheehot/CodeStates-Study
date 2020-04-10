a3b39050f2a70ea3182e119164164979a147a76b
TheJang <33803975+jangwonyoon@users.noreply.github.com>
2020-04-10 21:15:37 +0900

    Create 09-deepEquality.js

---
+++ _Algorithm/Immersive_algorithm/09-deepEquality.js
@@ -0,0 +1,28 @@
+/*
+ * 두 개의 객체가 주어졌을 떄, 이 두 객체가 deeply equal한지 아닌지를 return하는 함수를 작성하세요.
+ * 여기서 "deeply equal"이란, 두 객체가 같고, 각각 객체의 자식 객체의 구조가 같다는 것을 의미합니다.
+ *
+ * 예시 :
+ *
+ * deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}}); // true
+ * deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}}); // false
+ *
+ * 순환객체는 고려하지 않습니다.
+ * 순환객체 레퍼런스 : https://riptutorial.com/javascript/example/14476/cyclic-object-values
+ */
+
+const deepEquals = function (apple, orange) {
+  let result = false;
+  for (let key in apple) {
+    let appleValue = JSON.stringify(apple[key]);
+    let orangeValue = JSON.stringify(orange[key]);
+    console.log(appleValue);
+    console.log(orangeValue);
+    if (appleValue !== orangeValue) {
+      return result;
+    } else if (appleValue === orangeValue) {
+      return true;
+    }
+  }
+  return result;
+};
